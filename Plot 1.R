0267197b5001a2f828b4289b1a2c73d3eb176049
Tumisang-Liphoto <mrsechocha@gmail.com>
2021-12-08 13:17:16 +0200

    Plot 1

---
+++ Plot 1.R
@@ -0,0 +1,17 @@
+# read the dataset and create a subset
+
+dataDF <- read.table("C:/Users/Tumisang/Desktop/data/household_power_consumption.txt", sep = ";", header = TRUE)
+head(dataDF)
+dim(dataDF)
+DateData <- subset(dataDF , dataDF$Date == "1/2/2007" | dataDF$Date == "2/2/2007")
+head(DateData)
+dim(DateData)
+
+
+png(file="Plot 1.png")
+
+#create histogram
+hist(as.numeric(as.character(DateData$Global_active_power)),
+     col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+dev.off()
